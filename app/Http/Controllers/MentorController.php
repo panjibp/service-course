abcae4e11ea973c3c76e73f43c59a3e885daf758
panjibp <panji.bp.bp@gmail.com>
2021-04-05 16:13:06 +0700

    mentor endpoint completed

--- app/Http/Controllers/MentorController.php
+++ app/Http/Controllers/MentorController.php
@@ -8,6 +8,31 @@ use Illuminate\Support\Facades\Validator;
 
 class MentorController extends Controller
 {
+    public function index()
+    {
+        $mentors = Mentor::all();
+        return response()->json([
+            'status' => 'success',
+            'data' => $mentors
+        ]);
+    }
+
+    public function show($id)
+    {
+        $mentor = Mentor::find($id);
+        if (!$mentor) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'mentor not found'
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $mentor
+        ]);
+    }
+
     public function create(Request $request)
     {
         $rules = [
@@ -20,7 +45,6 @@ class MentorController extends Controller
         $data = $request->all();
 
         $validator = Validator::make($data, $rules);
-
         if ($validator->fails()) {
             return response()->json([
                 'status' => 'error',
@@ -31,5 +55,59 @@ class MentorController extends Controller
         $mentor = Mentor::create($data);
 
         return response()->json(['status' => 'success', 'data' => $mentor]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $rules = [
+            'nama' => 'string',
+            'profile' => 'url',
+            'profession' => 'string',
+            'email' => 'email'
+        ];
+
+        $data = $request->all();
+
+        $validator = Validator::make($data, $rules);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()
+            ], 400);
+        }
+
+        $mentor = Mentor::find($id);
+        if (!$mentor) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'mentor not found'
+            ], 404);
+        }
+
+        $mentor->fill($data);
+
+        $mentor->save();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $mentor
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        $mentor = Mentor::find($id);
+        if (!$mentor) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'mentor not found'
+            ], 404);
+        }
+
+        $mentor->delete();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'mentor deleted'
+        ]);
     }
 }

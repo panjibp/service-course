4ca9236378fcc4dfc0a071724f782bcc1146e0e8
panjibp <panji.bp.bp@gmail.com>
2021-04-11 23:53:47 +0700

    my course endpoint in progress

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\CourseController;
 use App\Http\Controllers\ChapterController;
 use App\Http\Controllers\LessonController;
 use App\Http\Controllers\ImageCourseController;
+use App\Http\Controllers\MyCourseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -45,3 +46,5 @@ Route::delete('lessons/{id}', [LessonController::class, 'destroy']);
 
 Route::post('image-courses', [ImageCourseController::class, 'create']);
 Route::delete('image-courses/{id}', [ImageCourseController::class, 'destroy']);
+
+Route::post('my-courses', [MyCourseController::class, 'create']);

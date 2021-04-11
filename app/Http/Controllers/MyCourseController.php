6e2cd03e7bd772c75cc8693f53b6211dde94f8a6
panjibp <panji.bp.bp@gmail.com>
2021-04-12 00:28:38 +0700

    my course endpoints finished

--- app/Http/Controllers/MyCourseController.php
+++ app/Http/Controllers/MyCourseController.php
@@ -9,6 +9,22 @@ use Illuminate\Support\Facades\Validator;
 
 class MyCourseController extends Controller
 {
+    public function index(Request $request)
+    {
+        $myCourses = MyCourse::query()->with('course');
+
+        $userId = $request->query('user_id');
+
+        $myCourses->when($userId, function ($query) use ($userId) {
+            return $query->where('user_id', '=', $userId);
+        });
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $myCourses->get()
+        ]);
+    }
+
     public function create(Request $request)
     {
         $rules = [
@@ -53,7 +69,7 @@ class MyCourseController extends Controller
         if ($isExistMyCourse) {
             return response()->json([
                 'status' => 'error',
-                'message' => 'user already take this course'
+                'message' => 'user has already taken this course'
             ], 409);
         }
 
